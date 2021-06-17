5d1e8b8a735d4c51d2ae42d6e0cc793b4105ee55
DeltaTeArch <86045081+DeltaTeArch@users.noreply.github.com>
2021-06-17 12:20:28 +0300

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,24 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.setBackgroundColor(5)
 tiles.setTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`heroWalkFront1`, SpriteKind.Player)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 scene.cameraFollowSprite(mySprite)
 game.onUpdate(function () {
