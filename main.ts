de0b637d2b766f3fcfb421571dc47aac06d5a3c6
DeltaTeArch <86045081+DeltaTeArch@users.noreply.github.com>
2021-06-17 12:04:49 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.baDing.play()
+})
 scene.setBackgroundColor(0)
 tiles.setTilemap(tilemap`level3`)
 let mySprite = sprites.create(assets.image`princessFront2`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
+scene.cameraFollowSprite(mySprite)
