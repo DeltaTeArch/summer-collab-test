cc5b6147fd478ef584ecf9c9e5b08233904f60b0
DeltaTeArch <86045081+DeltaTeArch@users.noreply.github.com>
2021-06-17 12:23:27 +0300

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -73,25 +73,6 @@ a a a . . 6 . . . . 6 . . . . .
 a a a a a a a a a a 6 . . . . . 
 . . . . . 6 6 6 6 6 . . . . . . 
 `;
-            case "image4":
-            case "myImage0":return img`
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 6 6 6 6 6 6 6 6 6 6 6 6 2 2 2 
-6 6 2 2 2 2 2 2 2 2 2 2 6 6 2 2 
-2 2 2 6 6 6 6 6 6 6 6 6 6 6 2 2 
-2 2 2 2 2 2 2 2 2 6 2 6 2 2 2 2 
-2 2 2 2 2 2 2 2 2 6 2 6 2 2 2 2 
-2 2 2 2 2 2 2 2 2 6 2 6 2 2 2 2 
-2 2 2 6 6 6 2 2 2 6 2 2 6 2 2 2 
-2 2 2 6 2 2 6 6 6 6 2 2 6 2 2 2 
-2 2 2 6 2 2 2 2 2 6 6 2 6 2 2 2 
-2 2 6 2 2 2 2 2 2 2 2 2 6 2 2 2 
-2 2 6 2 2 2 2 2 2 2 2 2 6 2 2 2 
-2 2 6 6 6 2 2 2 2 2 2 2 6 2 2 2 
-2 2 2 2 2 6 6 6 6 6 6 6 6 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`;
             case "image2":
             case "princessFront2":return img`
 . . . . . . 5 . 5 . . . . . . . 
@@ -130,6 +111,44 @@ c b b d 5 d c d 5 5 5 5 5 5 b .
 b b c c c d d d d 5 5 5 b b . . 
 . . . c c c c c c c c b b . . . 
 `;
+            case "image7":
+            case "heroWalkFront1":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image4":
+            case "myImage0":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 a a a a a a a a a a a a 7 7 7 
+a a 7 7 7 7 7 7 7 7 7 7 a a 7 7 
+7 7 7 a a a a a a a a a a a 7 7 
+7 7 7 7 7 7 7 7 7 a 5 a 7 7 7 7 
+7 7 7 7 7 7 7 7 7 a 5 a 7 7 7 7 
+7 7 7 7 7 7 7 7 7 a 5 a 7 7 7 7 
+7 7 7 5 5 5 7 7 7 a 5 5 5 7 7 7 
+7 7 7 5 5 5 a a a a 5 5 5 7 7 7 
+7 7 7 5 5 5 5 5 5 a a 5 5 7 7 7 
+7 7 6 5 5 5 5 5 5 5 5 5 5 7 7 7 
+7 7 6 5 5 5 5 5 5 5 5 5 5 7 7 7 
+7 7 6 6 6 5 5 5 5 5 5 5 5 7 7 7 
+7 7 7 7 7 5 5 5 5 5 5 5 5 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
             case "image6":
             case "bigCake":return img`
 ......................bbb.......
@@ -164,25 +183,6 @@ bbb55dd555555555555555ddddddbb3a
 ...............bbbbddd5d55d5b3ba
 ...................bbbbddddb3ba.
 .......................bbbaaaa..
-`;
-            case "image7":
-            case "heroWalkFront1":return img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -4,24 +4,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.setBackgroundColor(5)
 tiles.setTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`heroWalkFront1`, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Food)
+let mySprite2 = sprites.create(assets.image`bigCake`, SpriteKind.Food)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 scene.cameraFollowSprite(mySprite)
 game.onUpdate(function () {
