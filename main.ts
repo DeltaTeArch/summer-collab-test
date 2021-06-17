57bdcc5014e06e049e604da9a72f468f04e11d54
DeltaTeArch <86045081+DeltaTeArch@users.noreply.github.com>
2021-06-17 12:08:08 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,3 +6,6 @@ tiles.setTilemap(tilemap`level3`)
 let mySprite = sprites.create(assets.image`princessFront2`, SpriteKind.Player)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 scene.cameraFollowSprite(mySprite)
+game.onUpdate(function () {
+	
+})
