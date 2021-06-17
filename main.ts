ff7d184940aa40625bcca6e503d8258d1da70aff
DeltaTeArch <86045081+DeltaTeArch@users.noreply.github.com>
2021-06-17 12:13:49 +0300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     music.baDing.play()
 })
-scene.setBackgroundColor(0)
-tiles.setTilemap(tilemap`level3`)
-let mySprite = sprites.create(assets.image`princessFront2`, SpriteKind.Player)
+scene.setBackgroundColor(5)
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(assets.image`heroWalkFront1`, SpriteKind.Player)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 scene.cameraFollowSprite(mySprite)
 game.onUpdate(function () {
